86cacf4ed47294e3535f6bebb4678d2348a1b187
Joan Cifo <joancifo@gmail.com>
2024-10-15 19:15:29 +0200

    feat: auth livewire

--- routes/web.php
+++ routes/web.php
@@ -1,36 +1,15 @@
 <?php
 
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    $products = \App\Models\Product::query()
-        ->with('category')
-        ->get();
+Route::view('/', 'welcome');
 
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
 
-    return view('bienvenido', [
-        'products' => $products,
-        'categories' => \App\Models\Category::all(),
-    ]);
-});
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
 
-//Route::get('/products', [\App\Http\Controllers\ProductController::class, 'index']);
-Route::resource('products', \App\Http\Controllers\ProductController::class);
-Route::resource('categories', \App\Http\Controllers\CategoryController::class);
-Route::resource('users', UserController::class);
-
-Route::get('product-list', function(){
-    return \App\Models\Product::all();
-});
-
-Route::group(['prefix' => 'category', 'middleware' => [\App\Http\Middleware\EnsureHasSession::class]], function () {
-    Route::get('/{categoria}/{page}', function (string $categoria, string $page) {
-
-        dd($categoria, $page);
-    })->name('categoria.detail-page');
-});
-
-Route::post('/iniciar-sesion', [UserController::class, 'login'])->name('login');
-Route::get('iniciar-sesion', [UserController::class, 'loginForm']);
-
+require __DIR__.'/auth.php';
